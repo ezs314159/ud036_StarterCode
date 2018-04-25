735f11fa64034f6524ccb21c69c2b6d95b2556a3
John Zeiger <johnz@outdoorz.net>
2018-04-24 23:04:30 -0400

    add additional attributes to movies (not displayed on web page) and provide correct information for 4 movies.

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,14 +3,19 @@ from media import Movie
 from fresh_tomatoes import open_movies_page
 
 movies = []
-movies.append(Movie(title="Avatar",
-             trailer_youtube_url="tbd",
-             poster_image_url="tbd"))
-movies.append(Movie(title="Arrival",
-             trailer_youtube_url="tbd",
-             poster_image_url="tbd"))
-movies.append(Movie(title="Galaxy Quest",
-             trailer_youtube_url="tbd",
-             poster_image_url="tbd"))
+movies.append(Movie("Avatar",
+             trailer_youtube_url="https://www.youtube.com/watch?v=5PSNL1qE6VY",
+             poster_image_url="https://s3.amazonaws.com/ffe-ugc/intlportal2/dev-temp/en-US/__5603af15335dd-4a38d2eebe46ae0c1e7104d341e9ddef6b8c4794-00f8fcfabf9f0d18.jpg"))
+movies.append(Movie("Arrival",
+             trailer_youtube_url="https://www.youtube.com/watch?v=tFMo3UJ4B4g",
+             poster_image_url="https://ia.media-imdb.com/images/M/MV5BMTExMzU0ODcxNDheQTJeQWpwZ15BbWU4MDE1OTI4MzAy._V1_UX182_CR0,0,182,268_AL_.jpg"))
+movies.append(Movie("Galaxy Quest",
+             trailer_youtube_url="https://www.youtube.com/watch?v=VtHM77IRkus",
+             poster_image_url="http://img.moviepostershop.com/galaxy-quest-movie-poster-1999-1020253192.jpg"))
+movies.append(Movie("The Secret Life of Walter Mitty",
+             trailer_youtube_url="https://www.youtube.com/watch?v=HddkucqSzSM",
+             poster_image_url="http://www.impawards.com/2013/posters/secret_life_of_walter_mitty.jpg"))
 
+
+                    
 open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,7 +1,17 @@
 
 class Movie:
-    """ Class to save movie title and display attributes """
-    def __init__(self, title=None, trailer_youtube_url=None, poster_image_url=None):
+    """ Movie information including display attributes """
+    
+    def __init__(self,
+                 title,
+                 trailer_youtube_url=None,
+                 poster_image_url=None,
+                 run_time=None,
+                 synopsis=None,
+                 director=None):
         self.title = title
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
+        self.run_time = run_time
+        self.synopsis = synopsis
+        self.director = director
